1f3105400409e74f392284deecb69df632c9afe8
JenifferHerrera <jmm.herrera@yavirac.edu.ec>
2022-05-17 17:29:49 -0500

    job-care

---
+++ Server-Job/models/planesEmpleados.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+
+
+
+const PlanesEmpleadosShema = mongoose.Schema({
+    
+    tipoPlan: String,
+    valor: String,
+    fechaReporte: { type: Date, required: true, default: Date.now },
+    fechaFinalizaReporte: { type: Date, required: true, default: Date.now },
+    usuario: {
+        type: mongoose.Types.ObjectId, ref: 'Usuario'
+       },
+
+        //campos adicionales cuando el plan es pago=======
+        amount: String,
+        clientTransactionId:String,
+        optionalParameter1: String,
+        optionalParameter2: String,
+        reference: String,
+
+        fecha1:Date,
+        fecha2:Date
+
+})
+
+module.exports = mongoose.model('PlanesEmpleados',PlanesEmpleadosShema);
